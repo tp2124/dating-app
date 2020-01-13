4444b2663776021e67866bac79fc27ed9db756b3
Travis Primm <tp2124@gmail.com>
2020-01-12 17:18:04 -0800

    [angular] Angular DI + HTTP Client Using dependency Injection in `value.component` on HttpClient. Adding dependencies of the module to make Http calls.

--- src/DatingApp-SPA/src/app/value/value.component.ts
+++ src/DatingApp-SPA/src/app/value/value.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-value',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValueComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }

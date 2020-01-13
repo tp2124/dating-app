829d8b0f336f7d1728f248404e886c99a67ecd38
Travis Primm <tp2124@gmail.com>
2020-01-12 16:41:43 -0800

    [Working State] works with basic synchronous queries.

---
+++ src/DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET /values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            // This is doing a query on the DB and then we are storing it in the values variable.
+            var values = _context.Values.ToList(); 
+            return Ok(values);
+        }
+
+        // GET /values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(v => v.Id == id);
+            return Ok(value);
+        }
+
+        // private static readonly string[] Summaries = new[]
+        // {
+        //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        // };
+
+        // private readonly ILogger<WeatherForecastController> _logger;
+
+        // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+    }
+}

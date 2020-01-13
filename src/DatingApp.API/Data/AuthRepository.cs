f2c2de6979f1e5e89bc14f58d871a321782c310a
Travis Primm <tp2124@gmail.com>
2020-01-12 22:38:04 -0800

    [API + EF Core] Register() + Saving a User to DB Implementation of SHA hashing using IDisposable for clearing generated hash in memory.

---
+++ src/DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            // Using IDispose on sha512 to ensure that this generated hash is not left in memory for any security concerns.
+            // This will be utilized in the byte[] variables, but it will be cleaned up in memory.
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}

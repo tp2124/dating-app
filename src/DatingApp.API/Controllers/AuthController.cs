b02800a170c9a01f6cd1593a194aa2c5aff2620a
Travis Primm <tp2124@gmail.com>
2020-01-27 22:21:26 -0800

    [API] Creating JWToken Making a token to be able to have the User pass this up to the server without needing to validate anything with a Data backing service.

--- src/DatingApp.API/Controllers/AuthController.cs
+++ src/DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -12,12 +18,16 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo) {
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _config = config;
             _repo = repo;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(/*[FromBody] Used to parse data from body*/UserForRegisterDto userForRegisterDto) {
+        public async Task<IActionResult> Register(/*[FromBody] Used to parse data from body*/UserForRegisterDto userForRegisterDto)
+        {
             // This can be done if the [ApiController] is removed to allow for the same ModelState error messages
             // // Coming back from the requests
             // if (!ModelState.IsValid) 
@@ -25,11 +35,13 @@ namespace DatingApp.API.Controllers
 
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await _repo.UserExists(userForRegisterDto.Username)) {
+            if (await _repo.UserExists(userForRegisterDto.Username))
+            {
                 return BadRequest("Username already exists");
             }
 
-            var userToCreate = new User {
+            var userToCreate = new User
+            {
                 Username = userForRegisterDto.Username
             };
 
@@ -38,6 +50,41 @@ namespace DatingApp.API.Controllers
             // Tells the user where to access the newly created entity
             // return CreatedAtRoute()
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // Adding data for the token Payload
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            // Signing the token. Getting the content ready to be signed with the Secret's settings.
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Creating the actual signed Payload object.
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }

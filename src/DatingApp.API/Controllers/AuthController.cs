0626418003310207dd96391f08b403f4a12b0dba
Travis Primm <tp2124@gmail.com>
2020-01-27 21:12:04 -0800

    [API] DTO Introduction Adding initial DTO code to handle a new POST request coming in generic JSON format. + Adding general notes section.

--- src/DatingApp.API/Controllers/AuthController.cs
+++ src/DatingApp.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,20 +16,20 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password) {
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto) {
             // validate request as this data is coming from the user.
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await _repo.UserExists(username)) {
+            if (await _repo.UserExists(userForRegisterDto.Username)) {
                 return BadRequest("Username already exists");
             }
 
             var userToCreate = new User {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             // Tells the user where to access the newly created entity
             // return CreatedAtRoute()

8756337f1aaa7fc1cb84f862eda56662054d418e
tp2124 <tp2124@gmail.com>
2020-04-12 18:39:43 -0500

    [Angular] Handling custom routers + Guards + Handling properly if users go to the empty route. + Creating a Guard and having it utilize our AuthService to verify and AlertifyService to give feedback. ++ Routers are utilizing this Guard to protect routes.

--- docs/notes/mod_7_routing.md
+++ docs/notes/mod_7_routing.md
@@ -5,6 +5,10 @@
 # ASP.NET Core
 
 # Angular
+## Creating Guards
+1. mkdir `_guards`
+1. cd into `_guards` dir in new terminal (web site can still be running).
+1. Run: `ng g guard auth --skipTests`
 
 
 # Chrome Debugging
--- src/DatingApp-SPA/src/app/routes.ts
+++ src/DatingApp-SPA/src/app/routes.ts
@@ -3,11 +3,23 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'members', component: MemberListComponent},
-    { path: 'messages', component: MessagesComponent},
-    { path: 'lists', component: ListsComponent},
-    { path: '**', redirectTo: 'home', pathMatch: 'full'} // Handling all non-matched requests to go to 'home'
+    // These routes will get taken as soon as one of the paths get matched. It works from first
+    // declared down to last (where '**' is a wildcard trying to catch all).
+    { path: '', component: HomeComponent},
+    // Example of explicit Guard being used for a route.
+    // { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+    {
+        path: '', // This will match with localhost:4200/members, where it's really localhost:4200/''members
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'} // Handling all non-matched requests to go to 'home'
 ];

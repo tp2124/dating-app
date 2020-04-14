0a9580b183c326d8cd7ed5ea1ee72b23ea9894d2
tp2124 <tp2124@gmail.com>
2020-04-13 21:46:42 -0500

    [API+Notes] Use Authorized Get() & Adding UserController + Adding new UserController that is generally clean for access.

---
+++ src/DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+
+        public UsersController(IDatingRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers() {
+            return Ok(await _repo.GetUsers());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id) {
+            return Ok(await _repo.GetUser(id));
+        }
+    }
+}

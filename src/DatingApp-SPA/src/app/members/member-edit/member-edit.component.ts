042adcb23df50156b096e2175e118ed55e581862
tp2124 <tp2124@gmail.com>
2020-05-03 21:48:30 -0500

    [Angular] Pushing changes from FrontEnd to API Data + Adding a new function to the user service. + Utilizing existing services ++ Using AuthService for the decodedToken data. ++ Properly showing alertify data.

--- src/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ src/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -21,7 +23,10 @@ export class MemberEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute,
+              private alertify: AlertifyService,
+              private userService: UserService,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -31,9 +36,13 @@ export class MemberEditComponent implements OnInit {
 
   updateUser() {
     console.log(this.user);
-    this.alertify.success('Profile updated Successfully');
-    // This will make the form no longer dirty when this function is called for the HTML form this is embedded in.
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated Successfully');
+      // This will make the form no longer dirty when this function is called for the HTML form this is embedded in.
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }

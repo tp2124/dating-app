6069bb1065ced9537377d008e76a416a474753ed
tp2124 <tp2124@gmail.com>
2020-05-13 20:16:20 -0500

    [API] Creating PhotoDto + Uploading using Cloudinary + Adding Mappings + Adding IDatingRepository support for the Repo to get a single Photo from the DB + Created DTO for return item.

---
+++ src/DatingApp.API/Controllers/PhotosController.cs
@@ -0,0 +1,96 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API.Controllers
+{
+    // Every endpoint can only be used by authorized users.
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+        private Cloudinary _cloudinary;
+
+        public PhotosController(IDatingRepository repo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            _cloudinaryConfig = cloudinaryConfig;
+            _mapper = mapper;
+            _repo = repo;
+
+            Account acc = new Account(
+                cloudinaryConfig.Value.CloudName,
+                cloudinaryConfig.Value.ApiKey,
+                cloudinaryConfig.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult> GetPhoto(int id) {
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            // We don't want to return User data, which is a property of the Photo class.
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto) {
+            // Checks to make sure the user's token is matching the data that this request is trying to edit matches.
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(userId);
+
+            var file = photoForCreationDto.File;
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0) {
+                // Read the file into memory
+                using (var stream = file.OpenReadStream()) {
+                    var uploadParams = new ImageUploadParams() {
+                        File = new FileDescription(file.Name, stream),
+                        // This will create a transformation that will attempt to crop a 500x500 image around a face found in the picture.
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadParams);
+                }
+            }
+
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);
+
+            if (!userFromRepo.Photos.Any(u => u.IsMain)) {
+                photo.IsMain = true;
+            }
+
+            userFromRepo.Photos.Add(photo);
+
+            if (await _repo.SaveAll()) {
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new { userId = userId, id = photo.Id }, photoToReturn);
+            }
+
+            return BadRequest("Could not add the photo");
+        }
+    }
+}
